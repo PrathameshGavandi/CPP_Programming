4b46ca739deb8802790a63330435b83c28e2c34c
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-02-23 13:38:51 +0530

    Add files via upload

---
+++ ClassWork/Recursion/program690.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+void Display(int iNo)
+{
+    static int iCnt = 1;
+
+    if(iCnt <= iNo)
+    {
+        cout<<iCnt<<"\t";
+        iCnt++;
+
+        Display(iNo);
+    }
+    else
+    {
+        cout<<"\n";
+    }
+}
+int main()
+{
+
+    int iValue = 0;
+
+    cout<<"Enter number : ";
+    cin>>iValue;
+
+    Display(iValue);
+
+    return 0;
+}
