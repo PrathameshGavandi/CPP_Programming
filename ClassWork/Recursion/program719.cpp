2024ba9b8c6c666913a851d15672aaf670081c12
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-03-02 15:14:09 +0530

    Add files via upload

---
+++ ClassWork/Recursion/program719.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int CountCaptial(char *str)
+{
+    int iCount = 0;
+
+    while (*str != '\0')
+    {
+        if((*str >= 'A') && (*str <='Z'))
+        {
+            iCount++;
+        }
+        str++;
+    }
+
+    return iCount;
+    
+}
+
+int main()
+{
+    int iRet = 0;
+    char Arr[50] = { '\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = CountCaptial(Arr);
+
+    printf("Capital count is : %d\n",iRet);
+
+    return 0;
+}
